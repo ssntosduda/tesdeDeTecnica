0579a5af28f42c18ec5976f4e90f95bfaf95e2eb
ssntosduda <144685408+ssntosduda@users.noreply.github.com>
2024-09-03 16:31:37 -0300

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -1,4 +1,4 @@
-# Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+# 4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
 # • SP – R$67.836,43
 # • RJ – R$36.678,66
 # • MG – R$29.229,88
