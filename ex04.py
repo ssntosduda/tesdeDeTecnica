fd4c34bb6df87acdea7b86664270610388fbf3c3
ssntosduda <144685408+ssntosduda@users.noreply.github.com>
2024-09-03 16:30:46 -0300

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -1,4 +1,13 @@
-# Valores de faturamento mensal por estado
+# Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+# • SP – R$67.836,43
+# • RJ – R$36.678,66
+# • MG – R$29.229,88
+# • ES – R$27.165,48
+# • Outros – R$19.849,53
+
+# Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado 
+# teve dentro do valor total mensal da distribuidora.  
+
 faturamento_por_estado = {
     "SP": 67836.43,
     "RJ": 36678.66,
@@ -7,16 +16,20 @@ faturamento_por_estado = {
     "Outros": 19849.53
 }
 
-# Calcula o valor total de faturamento
 valor_total = sum(faturamento_por_estado.values())
 
-# Calcula o percentual de representação de cada estado
 percentuais = {}
 for estado, faturamento in faturamento_por_estado.items():
     percentual = (faturamento / valor_total) * 100
     percentuais[estado] = percentual
 
-# Mostra o resultado
 print("Percentual de representação por estado:")
 for estado, percentual in percentuais.items():
     print(f"{estado}: {percentual:.2f}%")
+
+# Percentual de representação por estado:
+# SP: 37.53%
+# RJ: 20.29%
+# MG: 16.17%
+# ES: 15.03%
+# Outros: 10.98%
