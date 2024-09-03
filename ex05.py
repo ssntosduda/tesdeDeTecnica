c37abb7453a6534466a4909c572e1cf4acdbc854
ssntosduda <144685408+ssntosduda@users.noreply.github.com>
2024-09-03 16:31:23 -0300

    Update ex05.py

--- ex05.py
+++ ex05.py
@@ -1,12 +1,17 @@
+# 5) Escreva um programa que inverta os caracteres de um string.
+
+# IMPORTANTE:
+# a) Essa string pode ser informada através de qualquer entrada de sua preferência ou 
+# pode ser previamente definida no código;
+# b) Evite usar funções prontas, como, por exemplo, reverse;
+
 print("Digite a string que deseja inverter:")
 entrada = input()
 
-# Inicializa a string invertida como uma string vazia
+
 string_invertida = ""
 
-# Percorre a string de trás para frente e constrói a string invertida
 for i in range(len(entrada) - 1, -1, -1):
     string_invertida += entrada[i]
 
-# Exibe a string invertida
 print("String invertida:", string_invertida)
